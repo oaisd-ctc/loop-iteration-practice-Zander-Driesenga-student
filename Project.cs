14010e53bab42b89fb5e0824916ae102f422ce06
Zander-Driesenga-student <zadr09b@student.oaisd.org>
2024-10-23 14:40:17 +0000

    started

---
+++ Project.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       Write1ThroughN_While(1);
+
+       
+    }
+    public static void Write1ThroughN_While(int n)
+       {
+        
+       }
+}
