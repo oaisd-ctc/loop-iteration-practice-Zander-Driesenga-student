28143609052cc81f54a111ef6d6d5608b00ede0b
Zander-Driesenga-student <zadr09b@student.oaisd.org>
2024-10-24 12:45:57 +0000

    started and made first loop

--- Project.cs
+++ Project.cs
@@ -2,15 +2,19 @@ using System;
 
 
 public class Program
-{
-    public static void Main(string[] args)
-    {
-       Write1ThroughN_While(1);
 
-       
-    }
-    public static void Write1ThroughN_While(int n)
-       {
-        
-       }
+{
+   public static void Main(string[] args)
+   {
+      Write1ThroughN_While(4);
+   }
+   public static void Write1ThroughN_While(int n)
+   {
+      int counter = 1;
+        while (counter < n)
+      {
+         Console.WriteLine(counter);
+         counter++;
+      }
+   }
 }
