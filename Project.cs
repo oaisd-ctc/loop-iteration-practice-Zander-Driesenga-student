01c9bebc2f4ff476271d3f12aee6b1719165e434
Zander-Driesenga-student <zadr09b@student.oaisd.org>
2024-10-24 13:09:10 +0000

    did two more loops

--- Project.cs
+++ Project.cs
@@ -6,7 +6,9 @@ public class Program
 {
    public static void Main(string[] args)
    {
-      Write1ThroughN_While(4);
+      Write1ThroughN_While(1);
+      Write1ThroughN_For(0);
+      WriteNThrough1_While(4);
    }
    public static void Write1ThroughN_While(int n)
    {
@@ -15,6 +17,22 @@ public class Program
       {
          Console.WriteLine(counter);
          counter++;
+      }
+   }
+   public static void Write1ThroughN_For(int n)
+   {
+      for (int i = 0; i < n; i++)
+      {
+         Console.WriteLine(i);
+      }
+   }
+   public static void WriteNThrough1_While(int n)
+   {
+      int counter = 0;
+        while (counter < n)
+      {
+         Console.WriteLine(n);
+         n--;
       }
    }
 }
