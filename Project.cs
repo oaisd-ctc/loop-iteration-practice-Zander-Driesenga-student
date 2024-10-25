29b59a1876162e65e6656478a27b8deef9928908
Zander-Driesenga-student <zadr09b@student.oaisd.org>
2024-10-25 14:40:59 +0000

    compleated two more loops and need to finish the find sum loop

--- Project.cs
+++ Project.cs
@@ -8,7 +8,10 @@ public class Program
    {
       Write1ThroughN_While(1);
       Write1ThroughN_For(0);
-      WriteNThrough1_While(4);
+      WriteNThrough1_While(0);
+      WriteNThrough1_For(0);
+      WriteEvensThrough100();
+      FindSum(1);
    }
    public static void Write1ThroughN_While(int n)
    {
@@ -34,5 +37,32 @@ public class Program
          Console.WriteLine(n);
          n--;
       }
+   }
+   public static void WriteNThrough1_For(int n)
+   {
+      for (int i = 0; i < n; n--)
+      {
+         Console.WriteLine(n);
+      }
+   }
+   public static void WriteEvensThrough100()
+   {
+      for (int i = 1; i < 101; i++)
+      {
+         int evenNumber = i % 2;
+         if (evenNumber == 0)
+         {
+            Console.WriteLine();
+         }
+      }
+   }
+   public static void FindSum(int n)
+   {
+      int sum = 0;
+      for (int i = 0; i < n; n--)
+      {
+         sum = n + sum;
+      }
+      Console.WriteLine(sum);
    }
 }
