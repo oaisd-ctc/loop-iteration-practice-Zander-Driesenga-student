14fa76ad92202dcf0143161abc2aa5745e10f4b9
Zander-Driesenga-student <zadr09b@student.oaisd.org>
2024-10-29 12:52:39 +0000

    #

--- Project.cs
+++ Project.cs
@@ -4,29 +4,29 @@ using System;
 public class Program
 
 {
-   int n;
+   int n = 1;
    public static void Main(string[] args)
    {
-      Write1ThroughN_While(5);
-      Write1ThroughN_For(0);
-      WriteNThrough1_While(0);
-      WriteNThrough1_For(0);
+      Write1ThroughN_While(-1);
+      Write1ThroughN_For(-1);
+      WriteNThrough1_While(-1);
+      WriteNThrough1_For(-1);
       WriteEvensThrough100();
-      FindSum(100);
-      FindSumOfEvenNumbers(2);
-      FindSumOfOddNumbers(1);
-      OutputRightTriangle(6);
+      FindSum(5);
+      FindSumOfEvenNumbers(10);
+      FindSumOfOddNumbers(10);
+      OutputRightTriangle(4);
    }
-   public static void OutOfRangeMessage()
+   public static void OutOfRangeMessage(int n)
    {
-      n
+      Console.WriteLine($"{n} is out of range");
    }
    public static void Write1ThroughN_While(int n)
    {
       int counter = 1;
       if (n < 1)
       {
-         OutOfRangeMessage();
+         OutOfRangeMessage(n);
       }
       while (counter <= n)
       {
@@ -36,25 +36,46 @@ public class Program
    }
    public static void Write1ThroughN_For(int n)
    {
+      if (n < 1)
+      {
+         OutOfRangeMessage(n);
+      }
+      else 
+      {
       for (int i = 0; i < n; i++)
       {
          Console.WriteLine(i);
+      }
       }
    }
    public static void WriteNThrough1_While(int n)
    {
       int counter = 0;
+      if (n < 1)
+      {
+         OutOfRangeMessage(n);
+      }
+      else
+      {
       while (counter < n)
       {
          Console.WriteLine(n);
          n--;
       }
+      }
    }
    public static void WriteNThrough1_For(int n)
    {
+      if (n < 1)
+      {
+         OutOfRangeMessage(n);
+      }
+      else
+      {
       for (int i = 0; i < n; n--)
       {
          Console.WriteLine(n);
+      }
       }
    }
    public static void WriteEvensThrough100()
